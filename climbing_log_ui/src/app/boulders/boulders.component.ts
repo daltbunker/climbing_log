eede4a03463871c0a7367df74e3b01af8938c621
Dalt Bunker <dalt@bunker>
2022-11-27 12:21:12 -0700

    updated boulder page to match routes page

--- climbing_log_ui/src/app/boulders/boulders.component.ts
+++ climbing_log_ui/src/app/boulders/boulders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchModel } from '../models/Search.model';
+import { RstApiService } from '../services/rst-api.service';
 
 @Component({
   selector: 'app-boulders',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BouldersComponent implements OnInit {
 
-  constructor() { }
+  public searchData: Array<SearchModel> = [];
+  public loading = [true, true];
+  constructor(private rstApiService: RstApiService) { }
 
   ngOnInit(): void {
+    this.rstApiService.getAllLocations('boulders')
+    .subscribe(allLocations => {
+      allLocations.sectors.forEach((sector: string) => {
+        this.searchData.push({value: sector, type: 'sector'})
+      })
+      allLocations.areas.forEach((area: string) => {
+        this.searchData.push({value: area, type: 'area'})
+      })
+      this.loading[0] = false;
+    });
+
+    this.rstApiService.getAllClimbs()
+    .subscribe(allClimbs => {
+      allClimbs.forEach((climb: { name: any; type: string}) => {
+        if (climb.type === 'BOULDER') {
+          this.searchData.push({value: climb.name, type: 'climb'})
+        }
+      })
+      this.loading[1] = false;
+    })
   }
 
 }
